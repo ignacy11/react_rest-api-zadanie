9f56a82420e2e92998d31589eb0fe47ed7e8391d
ignacybanet <ignacybanet@cyfroweszkoly.pl>
2025-09-24 01:20:38 +0200

    create basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,31 @@ import './App.css'
 
 function App() {
 
-  return (
-    <>
-        <h1>Rest API</h1>
-    </>
-  )
+    const BASE = 'https://api.thecatapi.com/v1/'
+
+    /*const fetchDataFromSwapi = async () => {
+        const data: Response = await fetch(`${BASE}people/1`)
+        console.log(data)
+    }*/
+
+    return (
+        <>
+            <header>
+                <h1>(something) Rest API</h1>
+                <a href="https://github.com/ignacy11/react_rest-api-zadanie">GitHub</a>
+            </header>
+            <main>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td>dane</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </main>
+
+        </>
+    )
 }
 
 export default App
