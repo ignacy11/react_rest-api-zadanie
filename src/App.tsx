614f172980b600ee7ba36013eecd275f6ce65340
ignacy <ignacy.banet@cyfroweszkoly.pl>
2025-09-24 10:36:52 +0200

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
 import './App.css'
+import Entry from "./Entry.tsx";
+import {useEffect} from "react";
+
+interface Cat {
+    image: string;
+    description: string;
+    temperament: string;
+    origin: string;
+}
 
 function App() {
 
-    const BASE = 'https://api.thecatapi.com/v1/'
+    const api_key = '&api_key=live_mWlGIlpVKE9qVjGDoFmRm3JaRA0tVMRsXu5xc6WrGhpFpeM5l6GpaP4NFcNgGJ3s'
 
-    /*const fetchDataFromSwapi = async () => {
-        const data: Response = await fetch(`${BASE}people/1`)
-        console.log(data)
-    }*/
+    const BASE = 'https://api.thecatapi.com/v1'
+    const fetchDataFromAPI = async () => {
+        try {
+            const data: Response = await fetch(`${BASE}people/1`)
+            const jsonData: Cat = await data.json()
+            console.log(jsonData)
+        } catch(error){
+            if(error instanceof Error){
+                console.log(error.message)
+                // TODO: wyświetlić w error box
+            }else {
+                console.log('Unknown error')
+                // TODO: wyświetlić w error box
+            }
+        }
+    }
 
+    useEffect(() => {
+        fetchDataFromAPI()
+
+    }, [])
     return (
         <>
             <header>
-                <h1>(something) Rest API</h1>
+                <section className="header-group">
+                    <h1>Cat Rest API</h1>
+                    <button onClick={fetchDataFromAPI}>Fetch data</button>
+                </section>
                 <a href="https://github.com/ignacy11/react_rest-api-zadanie">GitHub</a>
             </header>
             <main>
                 <table>
                     <tbody>
-                    <tr>
-                        <td>dane</td>
-                    </tr>
+                    <Entry/>
                     </tbody>
                 </table>
             </main>
---
+++ src/Entry.tsx
@@ -0,0 +1,14 @@
+const Entry = () => {
+    return (
+        <>
+            <tr>
+                <td>image</td>
+                <td>description</td>
+                <td>temperament</td>
+                <td>country of origin</td>
+            </tr>
+        </>
+    )
+}
+
+export default Entry
