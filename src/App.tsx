595007abd24403a5b5ef3567bb6fb1f1e4bc2b81
ignacybanet <ignacybanet@cyfroweszkoly.pl>
2025-09-28 00:26:16 +0200

    create basic api connectivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,97 @@
 import './App.css'
 import Entry from "./Entry.tsx";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 interface Cat {
+    url: string;
     image: string;
+    name: string;
     description: string;
     temperament: string;
     origin: string;
+}
+
+interface CatAPIResponse {
+    id: string;
+    url: string;
+    breeds: {
+        weight: {
+            imperial: string;
+            metric: string;
+        };
+        id: string;
+        name: string;
+        cfa_url: string;
+        vetstreet_url: string;
+        vcahospitals_url: string;
+        temperament: string;
+        origin: string;
+        country_codes: string;
+        country_code: string;
+        description: string;
+        life_span: string;
+        indoor: number;
+        lap: number;
+        alt_names: string;
+        adaptability: number;
+        affection_level: number;
+        child_friendly: number;
+        dog_friendly: number;
+        energy_level: number;
+        grooming: number;
+        health_issues: number;
+        intelligence: number;
+        shedding_level: number;
+        social_needs: number;
+        stranger_friendly: number;
+        vocalisation: number;
+        experimental: number;
+        hairless: number;
+        natural: number;
+        rare: number;
+        rex: number;
+        suppressed_tail: number;
+        short_legs: number;
+        wikipedia_url: string;
+        hypoallergenic: number;
+        reference_image_id: string;
+    }[];
+    width: number;
+    height: number;
 }
 
 function App() {
 
     const api_key = '&api_key=live_mWlGIlpVKE9qVjGDoFmRm3JaRA0tVMRsXu5xc6WrGhpFpeM5l6GpaP4NFcNgGJ3s'
 
-    const BASE = 'https://api.thecatapi.com/v1'
-    const fetchDataFromAPI = async () => {
-        try {
-            const data: Response = await fetch(`${BASE}people/1`)
-            const jsonData: Cat = await data.json()
-            console.log(jsonData)
-        } catch(error){
-            if(error instanceof Error){
-                console.log(error.message)
-                // TODO: wyświetlić w error box
-            }else {
-                console.log('Unknown error')
-                // TODO: wyświetlić w error box
-            }
-        }
+    const [cats, setCats] = useState<Cat[]>([])
+
+    const BASE = `https://api.thecatapi.com/v1/images/search?limit=10${api_key}`
+
+
+
+    const fetchDataFromAPI = () => {
+        fetch(BASE)
+            .then(response => response.json())
+            .then((data: CatAPIResponse[]) => {
+                const mappedData = data.map((item: CatAPIResponse) => ({
+                    url: item.id,
+                    image: item.url,
+                    name: item.breeds[0]?.name || 'Unknown',
+                    description: item.breeds[0]?.description || 'unknown',
+                    temperament: item.breeds[0]?.temperament || 'unknown',
+                    origin: item.breeds[0]?.origin || 'unknown',
+                }));
+                setCats(mappedData);
+                console.log(mappedData);
+            })
+            .catch(error => console.log(error))
     }
 
     useEffect(() => {
         fetchDataFromAPI()
-
     }, [])
+
     return (
         <>
             <header>
@@ -46,7 +104,25 @@ function App() {
             <main>
                 <table>
                     <tbody>
-                    <Entry/>
+                    <tr>
+                        <td>Image</td>
+                        <td>Name</td>
+                        <td>Description</td>
+                        <td>Temperament</td>
+                        <td>Country of origin</td>
+                    </tr>
+                    {
+                        cats.map((v) => (
+                            <Entry
+                                key = {v.url}
+                                image = {v.image}
+                                name = {v.name}
+                                description = {v.description}
+                                temperament = {v.temperament}
+                                origin= {v.origin}
+                            />)
+                        )
+                    }
                     </tbody>
                 </table>
             </main>
--- src/Entry.tsx
+++ src/Entry.tsx
@@ -1,11 +1,20 @@
-const Entry = () => {
+interface EntryProps {
+    image: string;
+    name: string;
+    description: string;
+    temperament: string;
+    origin: string;
+}
+
+const Entry = ({image, name, description, temperament, origin}: EntryProps ) => {
     return (
         <>
             <tr>
-                <td>image</td>
-                <td>description</td>
-                <td>temperament</td>
-                <td>country of origin</td>
+                <td><img src={image} alt={""}></img></td>
+                <td>{name}</td>
+                <td>{description}</td>
+                <td>{temperament}</td>
+                <td>{origin}</td>
             </tr>
         </>
     )
